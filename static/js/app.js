c03ada96ef7cab8440f159c51d660c7b7bbf26a8
selnoron <selnoron70@gmail.com>
2023-02-19 12:29:57 +0600

    js dz

---
+++ static/js/app.js
@@ -0,0 +1,85 @@
+// // 1
+// let sum = 0
+// for (let i = 1; i <= 50; i++) {
+//     sum += i
+// }
+// console.log(sum)
+
+// // 2
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// // 3
+// let cnt_chet = 0
+// let cnt_ne_chet = 0
+// let inp
+// for (let i = 0; i < 10; i++) {
+//     inp = parseInt(prompt())
+//     if (inp % 2 == 0) {
+//         cnt_chet++
+//     }
+//     else {
+//         cnt_ne_chet++
+//     }
+// }
+// console.log(cnt_chet)
+// console.log(cnt_ne_chet)
+
+// // 4
+// for (let i = 35; i <= 87; i++) {
+//     if (
+//         i % 7 == 1 ||
+//         i % 7 == 2 ||
+//         i % 7 == 5
+//     ) {
+//         console.log(i)
+//     }
+// }
+
+// // 5
+// let a = parseInt(prompt())
+// let n = parseInt(prompt())
+// let res = 1
+// for (let i = 0; i < n; i++) {
+//     res *= a 
+// }
+// console.log(res)
+
+// // 6
+// let hh = 1
+// let res = 1
+// while (hh <= 24) {
+//     hh++
+//     if (
+//         hh % 3 == 0 &&
+//         hh != 25
+//     ) {
+//         res *= 2
+//         console.log(res)
+//     }
+// }
+
+// // 7
+// let cnt = 0
+// let inp
+// for (let i = 0; i < 10; i++) {
+//     inp = parseInt(prompt())
+//     if (inp < 0) {
+//         cnt++
+//     }
+// }
+// console.log(cnt)
+
+// // 8
+// let m = parseInt(prompt())
+// let n = parseInt(prompt())
+// let res = 0
+// for (let a = 0; a <= n; a++) {
+//     if (a % 2 == 0) {
+//         res += a * a
+//     }
+// }
+// console.log(res)
